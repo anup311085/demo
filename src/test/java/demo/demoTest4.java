359b661197fef7f1d98f79778190b720187ac6c1
anup311085 <56433037+anup311085@users.noreply.github.com>
2019-10-23 12:32:40 +0530

    Create demoTest4.java

---
+++ src/test/java/demo/demoTest4.java
@@ -0,0 +1,27 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class demoTest4 {
+	
+	@Test
+	public void add() {
+		System.out.println("This is addition method4");
+	}
+	
+	@Test
+	public void sub() {
+		System.out.println("This is subtraction method4");
+	}
+	
+	@Test
+	public void mul() {
+		System.out.println("This is multiplication method4");
+	}
+	
+	@Test
+	public void div() {
+		System.out.println("This is division method4");
+	}
+
+}
