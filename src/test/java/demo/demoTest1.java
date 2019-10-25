997876bf390a4dc4637da2d0d0431886369f6178
Anup Verma <anup.verma@gspann.com>
2019-10-25 12:39:32 +0530

    allure

--- src/test/java/demo/demoTest1.java
+++ src/test/java/demo/demoTest1.java
@@ -7,6 +7,11 @@ import org.apache.log4j.PropertyConfigurator;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
+
 public class demoTest1 {
 		
 	 Logger logger = Logger.getLogger(demoTest1.class.getName());
@@ -31,18 +36,22 @@ public class demoTest1 {
 	}
 	
 	@Test(groups={"anup"})
+	@Severity(SeverityLevel.BLOCKER)
 	public void mul() {
 		logger.error("----------this is error messege-------");
 		System.out.println("This is multiplication method1");
 	}
 	
 	@Test
+	@Step("This is printing fatal message")
 	public void div() {
 		logger.fatal("----------this is fatal messege-------");
 		System.out.println("This is division method1");
 	}
 	
 	@Test
+	@Severity(SeverityLevel.NORMAL)
+	@Description("this is for testing purpose")
 	public void all_methods() {
 		logger.debug("----------this is fatal messege-------");
 		System.out.println("This is division all_methods");
