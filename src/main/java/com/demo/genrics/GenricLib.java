980a49830588ce61c7e6536537760d5bb851dccd
anup verma <anup311085@gmail.com>
2020-04-05 15:48:19 +0530

    2nd commit

--- src/main/java/com/demo/genrics/GenricLib.java
+++ src/main/java/com/demo/genrics/GenricLib.java
@@ -19,7 +19,7 @@ public class GenricLib {
 		value=p.getProperty(key);
 		}
 		catch(IOException e) {
-			System.out.println("file not found at specified location1");
+			System.out.println("file not found at specified location2");
 		}
 		
 		
